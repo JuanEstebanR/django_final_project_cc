024377b101b84ccfce687a442282ba4cdfd5f73c
JuanEstebanR <juan1010.jerm@gmail.com>
2024-04-02 16:40:10 -0500

    Revert "Register models in the admin site"
    This reverts commit e1ac6cad86980f896dd2d0bd528d3d502909b1c5.

--- menuapp/admin.py
+++ menuapp/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from .models import Ingredient, Recipe, MenuItem, Order
+
 # Register your models here.
-admin.site.register(Ingredient)
-admin.site.register(Recipe)
-admin.site.register(MenuItem)
-admin.site.register(Order)
