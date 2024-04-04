2c5c3807a4b19efd594c1fda27fa40f428fa4dd7
JuanEstebanR <juan1010.jerm@gmail.com>
2024-04-03 20:38:52 -0500

    fix project url

--- django_final_project_cc/urls.py
+++ django_final_project_cc/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('menu/', include('menuapp.urls')),
+    path('', include('menuapp.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
